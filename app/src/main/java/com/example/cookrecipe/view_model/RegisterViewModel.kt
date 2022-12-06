7ffb56aec4c44c5c47e0d69133cd655ef2c47b19
Estéban Ristich <esteban.ristich@protonmail.com>
2022-12-06 13:32:51 +0100

    style(viewmodel): add space in RegisterViewModel.kt

--- app/src/main/java/com/example/cookrecipe/view_model/RegisterViewModel.kt
+++ app/src/main/java/com/example/cookrecipe/view_model/RegisterViewModel.kt
@@ -4,12 +4,12 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class RegisterViewModel : ViewModel() {
-    var email:String = ""
-    var password:String = ""
-    var passwordConf:String= ""
+    var email: String = ""
+    var password: String = ""
+    var passwordConf: String= ""
 
-    fun registry(){
-        if (password == passwordConf){
+    fun registry() {
+        if (password == passwordConf) {
             Log.d("Register", "password = $password")
         }
         Log.d("Register", "NOTpassword = $password and $passwordConf")
