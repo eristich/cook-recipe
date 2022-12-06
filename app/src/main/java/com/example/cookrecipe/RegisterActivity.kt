38818a47bb6d94f234ffdd53051f8b10fc8c7f5f
Loïc <loic.papazian@outlook.fr>
2022-12-06 11:35:00 +0100

    Add mvvm_struct for register view

--- app/src/main/java/com/example/cookrecipe/RegisterActivity.kt
+++ app/src/main/java/com/example/cookrecipe/RegisterActivity.kt
@@ -2,10 +2,33 @@ package com.example.cookrecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.cookrecipe.databinding.ActivityRegisterBinding
+import com.example.cookrecipe.view_model.RegisterViewModel
+
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        // Set viewModelProvider
+        viewModel = ViewModelProvider(this)[RegisterViewModel::class.java]
+        // Set binding modelView
+        val binding: ActivityRegisterBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_register)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+
     }
+
+    fun onClick(view: View) {
+        viewModel.registry()
+    }
+
 }
---
+++ app/src/main/java/com/example/cookrecipe/view_model/RegisterViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.cookrecipe.view_model
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    var email:String = ""
+    var password:String = ""
+    var passwordConf:String= ""
+
+    fun registry(){
+        if (password == passwordConf){
+            Log.d("Register", "password = $password")
+        }
+        Log.d("Register", "NOTpassword = $password and $passwordConf")
+    }
+}
