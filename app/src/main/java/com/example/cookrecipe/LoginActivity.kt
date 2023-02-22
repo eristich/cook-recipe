1f4205e5a8dedf479943c0b8e00bd6ea4b1f3a0d
Loïc <narigane3@narigane.fr>
2023-02-22 22:54:13 +0100

    Add start activity register to click on new account

--- app/src/main/java/com/example/cookrecipe/LoginActivity.kt
+++ app/src/main/java/com/example/cookrecipe/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cookrecipe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +10,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnRegister: Button = findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
     }
 }
